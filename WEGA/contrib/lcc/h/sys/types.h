3139212ca50d639b27194ebf1026e35ff9b2ddbc
Oliver Lehmann <lehmann@ans-netz.de>
1990-03-21 20:33:24 +0000

    add LCC - a better C-Compiler for WEGA

---
+++ WEGA/contrib/lcc/h/sys/types.h
@@ -0,0 +1,15 @@
+typedef	struct { int r[1]; } *	physadr;
+typedef	unsigned long	daddr_t;
+typedef	char *		caddr_t;
+typedef	unsigned int	uint;
+typedef	unsigned short	ushort;
+typedef	ushort		ino_t;
+typedef char		cnt_t;
+typedef	long		time_t;
+typedef	int		label_t[10];
+typedef	short		dev_t;
+typedef	long		off_t;
+typedef	long		paddr_t;
+typedef	long		saddr_t;
+typedef	long		uaddr_t;
+typedef	long		key_t;
