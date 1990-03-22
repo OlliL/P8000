e8fa5c2017b8c50637b11500376361e00854ed15
Oliver Lehmann <lehmann@ans-netz.de>
1990-03-22 15:43:30 +0000

    add LCC - a better C-Compiler for WEGA

---
+++ WEGA/contrib/lcc/lib/libc/port/gen/l3.c
@@ -0,0 +1,40 @@
+/*
+ * Convert longs to and from 3-byte disk addresses
+ */
+void
+ltol3(cp, lp, n)
+char	*cp;
+long	*lp;
+int	n;
+{
+	register i;
+	register char *a, *b;
+
+	a = cp;
+	b = (char *)lp;
+	for(i=0; i < n; ++i) {
+		b++;
+		*a++ = *b++;
+		*a++ = *b++;
+		*a++ = *b++;
+	}
+}
+
+void
+l3tol(lp, cp, n)
+long	*lp;
+char	*cp;
+int	n;
+{
+	register i;
+	register char *a, *b;
+
+	a = (char *)lp;
+	b = cp;
+	for(i=0; i < n; ++i) {
+		*a++ = 0;
+		*a++ = *b++;
+		*a++ = *b++;
+		*a++ = *b++;
+	}
+}
