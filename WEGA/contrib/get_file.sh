7c117f83b2837dcb6443303e64f35a3c8f695eda
Oliver Lehmann <lehmann@ans-netz.de>
2006-02-13 19:28:05 +0000

    update

--- WEGA/contrib/get_file.sh
+++ WEGA/contrib/get_file.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 echo "getting file $1"
-kermit -q -b 9600 -l /dev/`who am i | awk '{print $2}'` -p n -g "$1"
+
+SPEED=`stty | grep speed | sed 's/speed \([0-9]*\) baud.*/\1/g'`
+TTY=`who am i | awk '{print $2}'`
+
+kermit -q -b ${SPEED}  -l /dev/${TTY} -p n -g "$1"
 if [ $? -ne 0 -o ! -s "$1" ] ; then
 	echo "file transfer failed"
 	exit 1
