4e10f7aa7c2be9db6b3f4af88371298af1c9df61
Oliver Lehmann <lehmann@ans-netz.de>
2009-08-07 20:08:59 +0000

    missing header

---
+++ WEGA/src/cmd/standalone/sa.timer/common/timer.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2009 Oliver Lehmann <oliver@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: timer.h,v 1.1 2009/08/07 20:08:59 olivleh1 Exp $
+ */
+
+#ifndef TIMER_H
+#define TIMER_H 1
+
+struct clock_type {
+        int             clock_addr;
+        int             clock_det_msk;
+        int             clock_det_str;
+        char            *clock_name;
+};
+
+char    estring[20];
+
+static struct clock_type clock_devs[] = {
+        { U130BASE, MSK_U130, STR_U130,
+            "RFT U130X RTC" },
+        { R421BASE, MSK_R421, STR_R421,
+            "Seiko Epson RTC-72421" },
+};
+
+#endif
