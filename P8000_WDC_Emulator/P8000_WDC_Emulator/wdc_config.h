d7771de55ec8b29fdb75ee580be57b49064f783a
Oliver Lehmann <lehmann@ans-netz.de>
2012-05-29 20:23:09 +0000

    Tab -> Space

--- P8000_WDC_Emulator/P8000_WDC_Emulator/wdc_config.h
+++ P8000_WDC_Emulator/P8000_WDC_Emulator/wdc_config.h
@@ -1,7 +1,7 @@
 /*
  * P8000 WDC Emulator
  *
- * $Id: wdc_config.h,v 1.1 2012/05/29 20:13:00 olivleh1 Exp $
+ * $Id: wdc_config.h,v 1.2 2012/05/29 20:23:09 olivleh1 Exp $
  * 
  */ 
 
@@ -9,21 +9,21 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-#define PORT_DATA		PORTA
-#define PIN_DATA		PINA
-#define DDR_DATA		DDRA
+#define PORT_DATA       PORTA
+#define PIN_DATA        PINA
+#define DDR_DATA        DDRA
 
-#define PORT_INFO		PORTC
-#define PIN_INFO		PINC
-#define DDR_INFO		DDRC
+#define PORT_INFO       PORTC
+#define PIN_INFO        PINC
+#define DDR_INFO        DDRC
 
-#define PIN_INFO_STATUS0	PINC0
-#define PIN_INFO_STATUS1	PINC1
-#define PIN_INFO_STATUS2	PINC2
-#define PIN_INFO_ASTB		PINC3
-#define PIN_INFO_TE			PINC4
-#define PIN_INFO_WDARDY		PINC5
-#define PIN_INFO_TR			PINC6
-#define PIN_INFO_RST		PINC7
+#define PIN_INFO_STATUS0    PINC0
+#define PIN_INFO_STATUS1    PINC1
+#define PIN_INFO_STATUS2    PINC2
+#define PIN_INFO_ASTB       PINC3
+#define PIN_INFO_TE         PINC4
+#define PIN_INFO_WDARDY     PINC5
+#define PIN_INFO_TR         PINC6
+#define PIN_INFO_RST        PINC7
 
 #endif /* CONFIG_H_ */
--- P8000_WDC_Emulator/P8000_WDC_Emulator/wdc_main.c
+++ P8000_WDC_Emulator/P8000_WDC_Emulator/wdc_main.c
@@ -1,7 +1,7 @@
 /*
  * P8000 WDC Emulator
  *
- * $Id: wdc_main.c,v 1.1 2012/05/29 20:13:00 olivleh1 Exp $
+ * $Id: wdc_main.c,v 1.2 2012/05/29 20:23:09 olivleh1 Exp $
  * 
  */ 
 
@@ -18,67 +18,67 @@ void atmega_setup(void);
 
 int main(void)
 {
-	uint8_t cmdbuffer[9];
-	uint8_t cmdcnt;
+    uint8_t cmdbuffer[9];
+    uint8_t cmdcnt;
 
-	atmega_setup();
-	
-	uart_puts_p(PSTR("P8000 WDC Emulator 1.0\n"));
+    atmega_setup();
+    
+    uart_puts_p(PSTR("P8000 WDC Emulator 1.0\n"));
 
-	while(1)
-	{
+    while(1)
+    {
 
-		while( !isset_info_reset() )
-		{
-			port_data_set( DATA_CLEAR );
-			port_info_set( INFO_CLEAR );
-		}
+        while( !isset_info_reset() )
+        {
+            port_data_set( DATA_CLEAR );
+            port_info_set( INFO_CLEAR );
+        }
 
-		configure_port_data_read();
-		cmdcnt = 0;
-		port_info_set( INFO_STAT_GCMD );
-		
-		while ( !isset_info_te() );
-		while ( !isset_info_wdardy() );
-		while ( cmdcnt < 9 )
-		{
-			while ( isset_info_wdardy() );
-			port_info_set( INFO_ASTB | INFO_STAT_GCMD );
-			cmdbuffer[cmdcnt] = port_data_get();
-			port_info_set( INFO_STAT_GCMD );
-			while ( !isset_info_wdardy() );
-			cmdcnt++;
-		}
-		
-		uart_puts_p(PSTR("CMD Buffer:"));
-		uart_putc_hex(cmdbuffer[0]);
-		uart_putc_hex(cmdbuffer[1]);
-		uart_putc_hex(cmdbuffer[2]);
-		uart_putc_hex(cmdbuffer[3]);
-		uart_putc_hex(cmdbuffer[4]);
-		uart_putc_hex(cmdbuffer[5]);
-		uart_putc_hex(cmdbuffer[6]);
-		uart_putc_hex(cmdbuffer[7]);
-		uart_putc_hex(cmdbuffer[8]);
-		uart_putc('\n');
-	}
-	return 0;
+        configure_port_data_read();
+        cmdcnt = 0;
+        port_info_set( INFO_STAT_GCMD );
+        
+        while ( !isset_info_te() );
+        while ( !isset_info_wdardy() );
+        while ( cmdcnt < 9 )
+        {
+            while ( isset_info_wdardy() );
+            port_info_set( INFO_ASTB | INFO_STAT_GCMD );
+            cmdbuffer[cmdcnt] = port_data_get();
+            port_info_set( INFO_STAT_GCMD );
+            while ( !isset_info_wdardy() );
+            cmdcnt++;
+        }
+        
+        uart_puts_p(PSTR("CMD Buffer:"));
+        uart_putc_hex(cmdbuffer[0]);
+        uart_putc_hex(cmdbuffer[1]);
+        uart_putc_hex(cmdbuffer[2]);
+        uart_putc_hex(cmdbuffer[3]);
+        uart_putc_hex(cmdbuffer[4]);
+        uart_putc_hex(cmdbuffer[5]);
+        uart_putc_hex(cmdbuffer[6]);
+        uart_putc_hex(cmdbuffer[7]);
+        uart_putc_hex(cmdbuffer[8]);
+        uart_putc('\n');
+    }
+    return 0;
 }
 
 void atmega_setup(void)
 {
-	set_sleep_mode(SLEEP_MODE_IDLE);
+    set_sleep_mode(SLEEP_MODE_IDLE);
 
-	configure_pin_status0();
-	configure_pin_status1();
-	configure_pin_status2();
-	configure_pin_astb();
-	configure_pin_te();
-	configure_pin_wdardy();
-	configure_pin_tr();
-	configure_pin_reset();
-	
-	configure_port_data_read();
-	
-	uart_init();
+    configure_pin_status0();
+    configure_pin_status1();
+    configure_pin_status2();
+    configure_pin_astb();
+    configure_pin_te();
+    configure_pin_wdardy();
+    configure_pin_tr();
+    configure_pin_reset();
+    
+    configure_port_data_read();
+    
+    uart_init();
 }
