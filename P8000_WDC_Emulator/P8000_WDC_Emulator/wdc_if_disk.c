4e85b8810982777b89f01a9dc1e99fd0aba98a14
Oliver Lehmann <lehmann@ans-netz.de>
2012-06-07 18:01:02 +0000

    add BSD license header

--- P8000_WDC_Emulator/P8000_WDC_Emulator/wdc_if_disk.c
+++ P8000_WDC_Emulator/P8000_WDC_Emulator/wdc_if_disk.c
@@ -1,8 +1,32 @@
+/*-
+ * Copyright (c) 2012 Oliver Lehmann
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer
+ *    in this position and unchanged.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 /*
- * P8000 WDC Emulator
- *
- * $Id: wdc_if_disk.c,v 1.8 2012/06/07 17:54:00 olivleh1 Exp $
- *
+ * $Id: wdc_if_disk.c,v 1.9 2012/06/07 18:01:02 olivleh1 Exp $
  */
 
 #include <stdint.h>
@@ -20,8 +44,8 @@ uint8_t wdc_init_sdcard()
     if ( errorcode ) {
         uart_putc_hex ( errorcode );
         uart_putc ( '\n' );
-    }    
-    return errorcode;        
+    }
+    return errorcode;
 }
 
 uint32_t wdc_sector2sdblock ( uint16_t req_cylinder, uint8_t req_head, uint8_t req_sector )
