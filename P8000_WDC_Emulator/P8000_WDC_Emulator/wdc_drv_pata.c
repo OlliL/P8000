5087b5d18e1e8e42595c84e1cd1df79d12ca9420
Oliver Lehmann <lehmann@ans-netz.de>
2012-06-12 19:58:36 +0000

    add empty files for the PATA implementation

---
+++ P8000_WDC_Emulator/P8000_WDC_Emulator/wdc_drv_pata.c
@@ -0,0 +1,6 @@
+/*
+ * wdc_drv_pata.c
+ *
+ * Created: 12.06.2012 21:56:52
+ *  Author: olivleh1
+ */ 
---
+++ P8000_WDC_Emulator/P8000_WDC_Emulator/wdc_drv_pata.h
@@ -0,0 +1,16 @@
+/*
+ * wdc_drv_pata.h
+ *
+ * Created: 12.06.2012 21:57:08
+ *  Author: olivleh1
+ */ 
+
+
+#ifndef WDC_DRV_PATA_H_
+#define WDC_DRV_PATA_H_
+
+
+
+
+
+#endif /* WDC_DRV_PATA_H_ */
